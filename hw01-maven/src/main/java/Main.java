5a90e76b8cd8d436dd4684dbfbcf0dfbaeced24f
Patruseva Maria <masapatruseva0830@mail.ru>
2025-06-18 00:46:12 +0300

    jsncjklsncsk

---
+++ hw01-maven/src/main/java/Main.java
@@ -0,0 +1,25 @@
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Multimap<String, String> multimap = ArrayListMultimap.create();
+        multimap.put("фрукт", "груша");
+        multimap.put("фрукт", "яблоко");
+        multimap.put("овощ", "помидор");
+        multimap.put("фрукт", "апельсин");
+        multimap.put("овощ", "огурец");
+
+        System.out.println(multimap.get("фрукт"));
+        System.out.println(multimap.get("овощ"));
+
+
+
+
+    }
+
+
+}
